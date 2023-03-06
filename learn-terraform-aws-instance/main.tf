66a18a4e61a7eb82905d39177a7c7cce2c2bec33
rounaknayee <rounaknayee007@gmail.com>
2023-03-06 14:31:56 -0500

    added gitignore

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+	Creator = "Rounaks Terraform"
+	Created_At = "March 6, 2023 14:00:00" 
+    	Name = var.instance_name
+  }
+}
+
